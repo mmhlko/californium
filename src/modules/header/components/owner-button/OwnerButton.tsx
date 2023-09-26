c0fcb2814c826151a3bc3c895657766e1ab082db
mmhlko <maximmhl@mail.ru>
2023-09-26 13:23:50 +0700

    Добавил web3

--- src/modules/header/components/owner-button/OwnerButton.tsx
+++ src/modules/header/components/owner-button/OwnerButton.tsx
@@ -2,17 +2,19 @@ import { useEffect } from "react";
 import { useAppSelector } from "../../../../storage/hookTypes";
 import { Button } from "../../../../ui/button/Button"
 import ContractLogoSvg from "../../assets/conract-logo.svg";
-
+import { metamaskConnection } from "../../../../../scripts/web3";
+import { useDispatch } from "react-redux";
+import { setWalletAction } from "../../../../storage/wallet/walletReducer";
 
 export const OwnerButton = () => {
 
     const ownerWallet = useAppSelector(state => state.wallet.walletNumber);
+    const dispatch = useDispatch()
     const cutWalletString = (wallet: string) => {
         if (wallet.length > 15) {
             return `${wallet}`.split("").slice(0, 6).join('') + "..." + `${wallet}`.split("").splice(wallet.length - 4).join('')
         } else return `${wallet}`
-    }
-
+    };
     useEffect(() => {
         cutWalletString(ownerWallet)
     }, [ownerWallet])
